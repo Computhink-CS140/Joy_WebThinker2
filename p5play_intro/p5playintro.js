a21a0ae6b1b3ea39e04601e31d71fd04b0ebb97b
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-06 10:47:22 +0000

    Jul 6, 2026, 10:47 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -43,8 +43,8 @@ function draw() {
     ball.vel.x += -1;
   }
 
-    if (ball.y < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
-    ball.vel.x += -1;
+    if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+    ball.vel.y += -1;
   }
 
 
