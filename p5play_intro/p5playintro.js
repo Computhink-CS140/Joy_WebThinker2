939c318341f50788fd32715089472cf81bab033a
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-06 10:45:11 +0000

    Jul 6, 2026, 10:45 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -39,7 +39,7 @@ function draw() {
   textSize(16);
   text("Ball: (" + int(ball.x) + "," + int(ball.y)  + ")");
   text("Mouse: (" + mouseX + "," + mouseY  + ")", 10, 40);
-
+  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2)
 
 
 
