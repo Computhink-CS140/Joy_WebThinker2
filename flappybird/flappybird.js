74e41a341cf2ec774540332fd36b08d7889771dd
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-20 11:02:31 +0000

    Jul 20, 2026, 11:02 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -3,9 +3,9 @@ let bird, floor
 let flapMidImg, bg, base
 
 function preload() {
-    flapMidImg = loadImage('flappybird/assets/yellowbird-midflap.png') 
-    bg = loadImage('flappybird/assets/background-day.png')
-    base = loadImage('flappybird/assets/base.png')
+    flapMidImg = loadImage('assets/yellowbird-midflap.png') 
+    bg = loadImage('assets/background-day.png')
+    base = loadImage('assets/base.png')
 }
 function setup(){
     new Canvas(400, 600)
