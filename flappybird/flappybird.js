a6c92f467dbb900041d3d1a7874b265809511201
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-20 10:46:35 +0000

    Jul 20, 2026, 10:46 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -24,7 +24,10 @@ function setup(){
 
     floor = new Sprite();
     floor.x = 200;
-    floor.y = -20
+    floor.y = -20;
+    floor.width = 400;
+    floor.height = 125;
+    
 }
 function draw(){
     image(bg, 0, 0, width, height)
