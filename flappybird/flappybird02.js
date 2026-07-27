6a9cfa374aa4194726676aebe3aaf3c14a0ae702
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-27 10:17:51 +0000

    Jul 27, 2026, 10:17 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -45,7 +45,7 @@ function draw(){
     }
     if (bird.vel.y > 0) {
         bird.img = flapUpImg;
-        bird.rotation = 45;
+        bird.rotation = 20;
     }
     if (bird.vel.y == 0){
         bird.img = flapMidImg;
