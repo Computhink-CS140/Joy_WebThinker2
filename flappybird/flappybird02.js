cab0a19c8781feba5ec71ca4f4c4640c53c20f89
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-27 10:37:38 +0000

    Jul 27, 2026, 10:37 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -71,6 +71,10 @@ function spawnPipePair(){
     let gap = 50;
     let midY = height/2
 
+
+    bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static')
+    bottomPipe.img = pipe;
+    
     bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static')
     bottomPipe.img = pipe;
 
