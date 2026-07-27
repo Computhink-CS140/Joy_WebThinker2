ddff3a0b03ada1058ab945c144fc9d3279c78de0
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-27 10:26:41 +0000

    Jul 27, 2026, 10:26 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -32,7 +32,7 @@ function setup(){
     floor.collider = "static";
     floor.img = base;
 
-    pipeGroup = new Group
+    pipeGroup = new Group();
 }
 function draw(){
     image(bg, 0, 0, width, height);
