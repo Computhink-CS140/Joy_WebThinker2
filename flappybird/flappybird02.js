17f71d201e93fa8b895aad396c492761c7901d36
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-03 10:49:32 +0000

    Aug 3, 2026, 10:49 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -56,6 +56,7 @@ function draw() {
     if (kb.presses('space') || mouse.presses()) {
             startGame = true;
             startScreenLabel.visible = false
+            bird.visible = true
         }
 
     if (startGame) {
