cfada3856f11883cb92211a52364e398bb385286
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-03 10:09:42 +0000

    Aug 3, 2026, 10:09 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -21,7 +21,7 @@ function setup(){
     bird.width = 30;
     bird.height = 30;
     bird.img = flapMidImg;
-    bird.height = 30;
+    bird.height = 20;
 
     bird.collider = "dynamic"
     bird.mass = 2;
