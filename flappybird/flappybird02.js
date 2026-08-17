fad8f93048eef802365fbaf8dee5de6513f5b003
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-17 09:38:31 +0000

    Aug 17, 2026, 9:38 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -11,7 +11,7 @@ let startScreenImg;
 let startGame = false;
 let score = 0;
 let numberImages = [];
-
+let scoreDigits;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png')
