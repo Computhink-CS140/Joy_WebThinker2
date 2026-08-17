16b131e44bffe375f04978688affbd4731bc6d17
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-17 09:39:33 +0000

    Aug 17, 2026, 9:39 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -22,7 +22,9 @@ function preload() {
     pipe = loadImage('assets/pipe-green.png')
     gameoverImg = loadImage('assets/gameover.png')
     startScreenImg = loadImage('assets/message.png')
-    
+    for (let i = 0; i < 10; i ++){
+        
+    }
 }
 function setup() {
     new Canvas(400, 600)
