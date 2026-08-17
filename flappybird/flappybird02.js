08711e5b17b66e1e5bff4b93392b866c16c95112
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-17 10:22:04 +0000

    Aug 17, 2026, 10:22 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -164,5 +164,6 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
     }
     function moveGroup(group, targetX, spacing) {
         let totalWidth = (group.length - 1) * spacing;
+        let startX = (targetX)
     }
 }
