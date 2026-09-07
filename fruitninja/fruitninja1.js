90247226bdb70b5eb7957a3b8b1d20e90eca4024
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 10:53:14 +0000

    Sep 7, 2026, 10:53 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -31,7 +31,7 @@ function draw () {
         spawnFruit();
     }
     if (mouse.pressing()) {
-        trail = new Sprite(mouse.x, mouse.y, 100);
+        trail = new Sprite(mouse.x, mouse.y, 10000);
         trail.collider = 'none';
         trail.color = "red";
         trail.life = 10;
