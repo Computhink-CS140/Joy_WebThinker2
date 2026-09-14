413709f83783b6718aaa6971cc0b1777f2f66907
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:27:23 +0000

    Sep 14, 2026, 10:27 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -83,9 +83,9 @@ function sliceFruit() {
         left.life = 30;
 
         let right = new fruitHalves.Sprite(x + 10, y, 40, 40);
-        left.img = fruitData.half1;
-        left.vel.x = -3;
-        left.vel.y = random(-5, -2);
+        right.img = fruitData.half1;
+        right.vel.x = -3;
+        right.vel.y = random(-5, -2);
         left.rotationSpeed = -5;
         left.life = 30;
     }
