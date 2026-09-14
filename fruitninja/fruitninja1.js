8f80bca14cdd934e3e5bb53509dcf7baff88517f
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:12:53 +0000

    Sep 14, 2026, 10:12 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -13,7 +13,7 @@ function preload() {
 
     let watermelon = {
         whole: loadImage('assets/watermelonwhole.png'),
-        half1: loadImage
+        half1: loadImage('assets/peachhalf.png'),
     };
 
     fruitTypes = [peach, watermelon];
